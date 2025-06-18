18115c50316bf7685f7dbbf92e8ae2c5bce8bf88
Anymok <secure-anymok@proton.me>
2025-06-18 15:30:55 +0200

    restart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AboutComponent } from './components/about/about.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule,
-    AboutComponent,
-    SkillsComponent,
-    ProjectsComponent,
-    ContactComponent
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio-ha';
+  protected title = 'portfolio-ha';
 }
