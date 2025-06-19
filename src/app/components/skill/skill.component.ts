a58a2b6c777afb73f31fbd1a3ebc99bed0f83ec6
Anymok <secure-anymok@proton.me>
2025-06-19 18:08:42 +0200

    skills done

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -7,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './skill.component.scss'
 })
 export class SkillComponent {
+  selectedSkillType: 'professional' | 'personal' = 'professional';
 
+  professionalSkills = [
+    { name: 'Angular', icon: '/icons/angular.png' },
+    { name: 'Django', icon: '/icons/django.svg' },
+    { name: 'Google Cloud Platform', icon: '/icons/gcp.png' },
+    { name: 'HTML', icon: '/icons/html5.png' },
+    { name: 'CSS', icon: '/icons/css3.png' },
+    { name: 'TypeScript', icon: '/icons/typescript.png' },
+    { name: 'Python', icon: '/icons/python.webp' },
+    { name: 'MySQL', icon: '/icons/mysql.png' },
+    { name: 'PostgreSQL', icon: '/icons/postgresql.png' },
+    { name: 'Jenkins', icon: '/icons/jenkins.png' },
+    { name: 'Bitbucket', icon: '/icons/bitbucket.webp' },
+
+  ];
+
+  personalSkills = [
+    { name: 'React', icon: '/icons/react.png' },
+    { name: 'Ionic', icon: '/icons/ionic.webp' },
+    { name: 'Spring Boot', icon: '/icons/springboot.png' },
+    { name: 'Java', icon: '/icons/java.svg' },
+    { name: 'Node.js', icon: '/icons/nodejs.png' },
+    { name: 'GitHub Actions', icon: '/icons/github.png' },
+    { name: 'GitLab CI/CD', icon: '/icons/gitlab.png' },
+    { name: 'Docker', icon: '/icons/docker.webp' },
+  ];
+
+  switchSkillType(type: 'professional' | 'personal') {
+    this.selectedSkillType = type;
+  }
+
+  get currentSkills() {
+    return this.selectedSkillType === 'professional' ? this.professionalSkills : this.personalSkills;
+  }
 }
