f42e8df78f5435ee5ca7d7a797dd8cc89e7794f4
Anymok <secure-anymok@proton.me>
2025-06-22 20:42:21 +0200

    correction

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -34,34 +34,34 @@ export class ProjectComponent {
       disabled: false,
       type: 'solo',
       features: [
-        "Optimisation des performances avec lazy loading",
-        "Création d'animations fluides et performantes",
+        "Optimisation des performances grâce au lazy loading",
+        "Création d’animations fluides et performantes",
         "Design moderne avec glassmorphism",
         "Responsive design pour tous les appareils"
       ],
       architecture: [
-        "Application en Angular",
+        "Application développée en Angular",
         "Déploiement sur Vercel",
       ]
     },
     {
       id: 2,
       title: "Ubeer",
-      description: "Ubeer est fortement inspiré de Uber Eats. C'est une application monolithique web de gestion de commandes pour les brasseries.",
+      description: "Ubeer est fortement inspirée d'Uber Eats. C'est une application web monolithique de gestion de commandes pour les brasseries.",
       repository: "https://github.com/orgs/ubeer-aw/repositories",
       technologies: ["React", "Spring Boot", "MySQL","Vercel", "VPS OVH", "Docker"],
       url: "https://ubeer-front.vercel.app/",
       disabled: false,
       type: 'solo',
       features: [
-        "Système d'authentification utilisateur (Auth0)",
+        "Système d’authentification utilisateur (Auth0)",
         "Catalogue de produits avec filtres de recherche",
-        "Interface d'administration pour les brasseries",
+        "Interface d’administration pour les brasseries",
       ],
       architecture: [
-        "Application en React",
+        "Application développée en React",
         "Application monolithique avec Spring Boot",
-        "Connection avec auth0",
+        "Connexion avec Auth0",
         "Base de données MySQL pour la persistance des données",
         "Déploiement sur Vercel"
       ]
@@ -69,21 +69,21 @@ export class ProjectComponent {
     {
       id: 3,
       title: "Archi Flower",
-      description: "Archi Flower est une application micro-services avec Ionic (mobile et web) qui permet de commander des fleurs en ligne.",
+      description: "Archi Flower est une application en microservices développée avec Ionic (mobile et web), qui permet de commander des fleurs en ligne.",
       repository: "https://gitlab.com/poc-archi-flower",
       technologies: ["Ionic", "Spring boot", "MySQL", "Vercel", "VPS OVH", "Docker", "Gitlab CI/CD"],
       url: "https://archiflower-front.vercel.app/",
       disabled: false,
       type: 'solo',
       features: [
-        "Parcour du catalogue de fleurs",
-        "Commandez des fleurs",
-        "Historique de commandes",
-        "Mode sombre/clair",
+        "Parcours du catalogue de fleurs",
+        "Commande de fleurs en ligne",
+        "Historique des commandes",
+        "Mode sombre / clair",
       ],
       architecture: [
-        "Application en Ionic",
-        "Application micro-services avec Spring Boot",
+        "Application développée avec Ionic",
+        "Architecture microservices avec Spring Boot",
         "Base de données MySQL pour la persistance des données",
         "Déploiement sur Vercel"
       ]
@@ -91,7 +91,7 @@ export class ProjectComponent {
     {
       id: 4,
       title: "Pomodoro",
-      description: "Pomodoro est une application web qui permet de gérer son temps avec un chronomètre. Il est possible de personnaliser le chronomètre et de voir l'historique des dernières sessions.",
+      description: "Pomodoro est une application web qui permet de gérer son temps à l’aide d’un chronomètre. Il est possible de personnaliser le minuteur et de consulter l’historique des dernières sessions.",
       repository: "https://gitlab.com/pomodoro-ra",
       technologies: ["React", "Spring Boot", "MySQL", "Vercel", "VPS OVH", "Docker", "Gitlab CI/CD"],
       url: "https://pomodoro-front-gamma.vercel.app/",
@@ -102,16 +102,16 @@ export class ProjectComponent {
         "Historique des sessions",
       ],
       architecture: [
-        "Application en React",
+        "Application développée avec React",
         "Application monolithique avec Spring Boot",
-        "Base de données MySQL pour l'historisation",
+        "Base de données MySQL pour l’historisation",
         "Déploiement sur Vercel"
       ]
     },
     {
       id: 5,
       title: "PayeTonKawa",
-      description: "PayeTonKawa est une application web qui permet d'acheter du café en ligne.",
+      description: "PayeTonKawa est une application web qui permet d’acheter du café en ligne.",
       repository: "https://gitlab.com/payetonkawa-varea",
       technologies: ["React", "Node.js", "Tailwind CSS", "Google Cloud Platform", "Docker", "Gitlab CI/CD"],
       url: "https://payetonkawa-front.vercel.app/",
@@ -121,9 +121,9 @@ export class ProjectComponent {
         "",
       ],
       architecture: [
-        "Application en React",
+        "Application développée avec React",
         "Application monolithique avec Node.js",
-        "Connexion avec keycloak",
+        "Connexion avec Keycloak",
         "Base de données MySQL pour la persistance des données",
         "Déploiement sur Google Cloud Platform"
       ]
